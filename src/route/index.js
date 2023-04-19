ef2d41fbfacf8ffbb776c0fadcea857687ae5bef
Mobmaster99 <arrrrrtyom@gmail.com>
2023-04-19 17:19:47 +0300

    aaa

--- src/route/index.js
+++ src/route/index.js
@@ -5,6 +5,35 @@ const router = express.Router()
 
 // ================================================================
 
+var header = {
+  name: {
+    firstname: 'Dmytro',
+    lastname: 'Ivanov',
+  },
+  position: 'Junior Fullstack JS Developer',
+  salary: '600$ per month',
+  address: 'Київ, Хрещатик 5',
+}
+
+var footer = {
+  social: {
+    email: {
+      text: 'dmytro@mail.com',
+      href: 'mailto:dmytro@mail.com',
+    },
+
+    phone: {
+      text: '+380670000123',
+      href: 'tel:+380670000123',
+    },
+
+    linkedin: {
+      text: 'LinkedIn',
+      href: 'https://www.linkedin.com/in/dmytro-test',
+    },
+  },
+}
+
 // router.get Створює нам один ентпоїнт
 
 //           ↙ тут вводимо шлях (PATH) до сторінки
@@ -23,9 +52,217 @@ router.get('/summary', function (req, res) {
   //             ↙ cюди вводимо назву файлу з сontainer
   res.render('summary', {
     // ↙ сюди вводимо JSON дані
+
+    page: {
+      title: 'Resume / summary',
+    },
+
+    header,
+
+    main: {
+      summary: {
+        title: 'Summary',
+        text: `Open-minded for new technologies, with 1 years of experience in development. Whenever I start to
+            work on a new project I learn the domain and try to understand the idea of the project. Good team
+            player, every colleague is a friend to me.`,
+      },
+
+      experience: {
+        title: 'Other experience',
+        text: `Pet project for parsing sport betting data from different platforms ( odds ) and sport statistics (
+              tournament position, goals etc), analyzing by simple mathematics models and preparing probability
+              for such events like: money line - first win / draw / second win, totals etc.`,
+      },
+    },
+
+    footer,
   })
 })
 
+router.get('/skills', function (req, res) {
+  //             ↙ cюди вводимо назву файлу з сontainer
+  res.render('skills', {
+    // ↙ сюди вводимо JSON дані
+
+    page: {
+      title: 'Resume / skills',
+    },
+
+    header,
+
+    main: {
+      skills: [
+        {
+          name: 'HTML',
+          point: 10,
+          isTop: true,
+        },
+        {
+          name: 'Handlebars',
+          point: 8,
+        },
+        {
+          name: 'VS Code',
+          point: 10,
+          isTop: true,
+        },
+        {
+          name: 'Git & Terminal',
+          point: 7,
+          isTop: false,
+        },
+        {
+          name: 'NPM',
+          point: 6,
+          isTop: false,
+        },
+        {
+          name: 'React.js',
+          point: 0,
+        },
+        {
+          name: 'PHP',
+          point: null,
+        },
+      ],
+
+      hobbies: [
+        {
+          name:'Фотографія',
+          isMain: true,
+        },
+        {
+          name: 'Теніс',
+          isMain: false,
+        },
+        {
+          name: 'Стрибки з парашутом',
+          isMain: false,
+        },
+      ],
+    },
+
+    footer,
+  })
+})
+
+router.get('/education', function (req, res) {
+  //             ↙ cюди вводимо назву файлу з сontainer
+  res.render('education', {
+    // ↙ сюди вводимо JSON дані
+
+    page: {
+      title: 'Resume / education',
+    },
+
+    header,
+
+    main: {
+      education: [
+        {
+          name: 'сш №2 м.Конотоп',
+          isEnd: true,
+        },
+        {
+          name: 'КПІ',
+          isEnd: true,
+        },
+        {
+          name: 'Stanford University',
+          isEnd: false,
+        },
+        {
+          name: 'Harvard University',
+          isEnd: false,
+        },
+      ],
+
+      certificates: [
+        {
+          name:'The European Language Certificates',
+          id: 56,
+        },
+        {
+          name: 'Educational Testing Service Global',
+          id: 88,
+        },
+        {
+          name: 'Pearson Test of English General',
+          id: 9,
+        },
+      ],
+    },
+
+    footer,
+  })
+})
+
+router.get('/work', function (req, res) {
+  //             ↙ cюди вводимо назву файлу з сontainer
+  res.render('work', {
+    // ↙ сюди вводимо JSON дані
+    layout: 'big',
+
+    page: {
+      title: 'Resume / work',
+    },
+
+    header,
+
+    main: {
+      works: [
+        {
+          position: 'Junior Fullstack Developer',
+          company: {
+            name: 'IT Brains',
+            url: null,
+          },
+          duration: {
+            from: '10.10.2022',
+            to: null,
+          },
+          projectAmount: 3,
+          projects: [
+            {
+              name: 'Crypto World',
+              url: 'https://www.youtube.com/',
+              about: `First Trustworthy Review Platform, Built With Blockchain Technology.
+              Crypto World is a review platform that aims to curate and reward user
+              feedback on products and services by leveraging blockchain technology.
+              Crypto World encourages independent authors to write reviews and attach
+              ratings to them. As posts are made, Crypto World aggregates, averages,
+              and publicly displays corresponding scores and rankings to their platform`,
+              stacks: [
+                {
+                  name: 'React.js',
+                },
+                {
+                  name: 'HTML/CSS',
+                },
+                {
+                  name: 'Node.js',
+                },
+              ],
+              awards: [
+                {
+                  name: 'Starship X',
+                },
+                {
+                  name: 'Raptor 12',
+                },
+              ],
+                stackAmount: 5,
+                awardAmount: 25,
+            },
+          ],
+        },
+
+      ],
+    },
+
+    footer,
+  })
+})
 // ================================================================
 
 // Підключаємо роутер до бек-енду
